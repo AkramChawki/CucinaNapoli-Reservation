d089ba629b14d0fba15323a0ef138fb5880f3a0c
AkramChawki <akramchawki01@gmail.com>
2023-11-30 19:08:49 +0100

    prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,7 +45,15 @@
     @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
     @inertiaHead
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NPSVK7XBH1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-NPSVK7XBH1');
+</script>
 <body class="font-sans antialiased">
     @inertia
 </body>
